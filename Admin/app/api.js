9d9a7444d6bc90247d98d2326d031deb3dd7f6af
devilra <rockerraja906@gmail.com>
2025-10-27 19:10:20 +0530

    update homepage

--- Admin/app/api.js
+++ Admin/app/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT}/api`,
+  //baseURL: `${process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT}/api`,
   //baseURL: `${process.env.NEXT_PUBLIC_API_URL_PRODUCTION}/api`,
-  //baseURL: `${process.env.NEXT_PUBLIC_API_URL_CPANEL_HOST}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL_CPANEL_HOST}/api`,
   withCredentials: true,
 });
 
